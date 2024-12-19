01eb4234ce60854d3f56c6283afa561a901ce38c
MrHanSolo123 <rohantrex@126.com>
2024-12-19 22:42:01 +0800

    added sql for headline

--- atguigu-headline/src/com/atguigu/headline/dao/impl/NewsHeadlineDaoImpl.java
+++ atguigu-headline/src/com/atguigu/headline/dao/impl/NewsHeadlineDaoImpl.java
@@ -1,15 +1,36 @@
 package com.atguigu.headline.dao.impl;
 
+import com.atguigu.headline.dao.BaseDao;
 import com.atguigu.headline.dao.NewsHeadlinDao;
 import com.atguigu.headline.pojo.vo.HeadlinePageVo;
 import com.atguigu.headline.pojo.vo.HeadlineQueryVo;
 
 import java.util.List;
 
-public class NewsHeadlineDaoImpl implements NewsHeadlinDao {
+public class NewsHeadlineDaoImpl extends BaseDao implements NewsHeadlinDao {
     @Override
     public List<HeadlinePageVo> findPageList(HeadlineQueryVo headlineQueryVo) {
-        return List.of();
+        String sql = """
+                select 
+                    hid,
+                    title,
+                    page_views pageViews,
+                    TIMESTAMPDIFF(HOUR,create_time,now()) pastHours,
+                    publisher
+                from
+                    news_headline
+                where
+                    is_deleted = 0
+                """;
+        if (headlineQueryVo.getType() != 0) {
+            sql = sql.concat(" and type = ? ");
+        }
+
+        if (headlineQueryVo.getKeyWords() != null && !headlineQueryVo.getKeyWords().equals("")) {
+
+        }
+
+        return null;
     }
 
     @Override
